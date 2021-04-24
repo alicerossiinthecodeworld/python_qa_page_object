1d56e819f1372caca208f979c5fb5078f560d46c
mchirkov <mchirkov@ivi.ru>
2021-04-24 11:52:01 +0300

    Implementing first PO methods

--- page_objects/MainPage.py
+++ page_objects/MainPage.py
@@ -1,2 +1,14 @@
+from selenium.webdriver.common.by import By
+
+
 class MainPage:
-    pass
+
+    def __init__(self, browser):
+        self.browser = browser
+
+    def click_featured_product(self, number):
+        index = number - 1
+        feature_product = self.browser.find_elements(By.CSS_SELECTOR, "#content > div.row .product-layout")[index]
+        product_name = feature_product.find_element(By.CSS_SELECTOR, ".caption h4 a").text
+        feature_product.click()
+        return product_name
--- tests/test_example.py
+++ tests/test_example.py
@@ -4,20 +4,19 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+from page_objects.MainPage import MainPage
+from page_objects.UserPage import UserPage
+
 
 def test_add_to_wish_list(browser):
     # Клик по первому товару и сохранение имени
-    feature_product = browser.find_elements(By.CSS_SELECTOR, "#content > div.row .product-layout")[0]
-    product_name = feature_product.find_element(By.CSS_SELECTOR, ".caption h4 a").text
-    feature_product.click()
+    product_name = MainPage(browser).click_featured_product(1)
     # Добавление товара в вишлист пользователя
     browser.find_element(By.CSS_SELECTOR, "[data-original-title='Add to Wish List']").click()
     # Переход по ссылке в алерте
     browser.find_element(By.CSS_SELECTOR, ".alert-success").find_element(By.LINK_TEXT, 'login').click()
     # Авторизация пользователем
-    browser.find_element(By.CSS_SELECTOR, "#input-email").send_keys("test2@mail.ru")
-    browser.find_element(By.CSS_SELECTOR, "#input-password").send_keys("test")
-    browser.find_element(By.CSS_SELECTOR, "input[value=Login]").click()
+    UserPage(browser).login_with("test2@mail.ru", "test")
     # Перейти в вишлист раздел пользователя
     browser.find_element(By.LINK_TEXT, 'Wish List').click()
     # Проверка наличия ссылки на продукт
@@ -26,9 +25,7 @@ def test_add_to_wish_list(browser):
 
 def test_add_to_cart(browser):
     # Клик по первому товару и сохранение имени
-    feature_product = browser.find_elements(By.CSS_SELECTOR, "#content > div.row .product-layout")[1]
-    product_name = feature_product.find_element(By.CSS_SELECTOR, ".caption h4 a").text
-    feature_product.click()
+    product_name = MainPage(browser).click_featured_product(1)
     # Добавление товара в корзину пользователя
     time.sleep(1)  # Page loading problem
     browser.find_element(By.CSS_SELECTOR, "#button-cart").click()
@@ -37,18 +34,14 @@ def test_add_to_cart(browser):
     WebDriverWait(browser, 5).until(EC.visibility_of_element_located((By.LINK_TEXT, product_name)))
     browser.find_element(By.CSS_SELECTOR, ".buttons").find_element(By.LINK_TEXT, "Checkout").click()
     # Авторизация пользователем
-    browser.find_element(By.CSS_SELECTOR, "#input-email").send_keys("test2@mail.ru")
-    browser.find_element(By.CSS_SELECTOR, "#input-password").send_keys("test")
-    browser.find_element(By.CSS_SELECTOR, "input[value=Login]").click()
+    UserPage(browser).login_with("test2@mail.ru", "test")
     # Проверка наличия ссылки на продукт
     WebDriverWait(browser, 5).until(EC.visibility_of_element_located((By.ID, "payment-new")))
 
 
 def test_add_to_cart_from_comparison(browser):
     # Клик по первому товару и сохранение имени
-    feature_product = browser.find_elements(By.CSS_SELECTOR, "#content > div.row .product-layout")[0]
-    product_name = feature_product.find_element(By.CSS_SELECTOR, ".caption h4 a").text
-    feature_product.click()
+    product_name = MainPage(browser).click_featured_product(1)
     # Добавление товара в список сравнения пользователя
     browser.find_element(By.CSS_SELECTOR, "[data-original-title='Compare this Product']").click()
     # Переход по ссылке в алерте
@@ -63,8 +56,6 @@ def test_add_to_cart_from_comparison(browser):
     # Клик по кнопке checkout
     browser.find_element(By.CSS_SELECTOR, ".buttons").find_element(By.LINK_TEXT, "Checkout").click()
     # Авторизация пользователем
-    browser.find_element(By.CSS_SELECTOR, "#input-email").send_keys("test2@mail.ru")
-    browser.find_element(By.CSS_SELECTOR, "#input-password").send_keys("test")
-    browser.find_element(By.CSS_SELECTOR, "input[value=Login]").click()
+    UserPage(browser).login_with("test2@mail.ru", "test")
     # Проверка наличия ссылки на продукт
     WebDriverWait(browser, 5).until(EC.visibility_of_element_located((By.ID, "payment-new")))
